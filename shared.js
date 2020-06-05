5cf7ce43eb16068bb6db05d4e9e23a94df928f4f
Filip Kaczmarek <61003790+FilipKaczmarek@users.noreply.github.com>
2020-06-05 14:29:51 +0200

    Add files via upload

---
+++ shared.js
@@ -0,0 +1,32 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal")
+var modalNoButton = document.querySelector(".modal__action--negative");
+var selectPlanButton = document.querySelectorAll(".plan__list button");
+var toggleButton = document.querySelector (".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+
+// console.log(backdrop);
+for (var i = 0; i < selectPlanButton.length; i++) {
+    selectPlanButton[i].addEventListener("click", function() {
+        modal.style.display = "block";
+        backdrop.style.display = "block";
+    });
+}
+
+backdrop.addEventListener("click", function() {
+    mobileNav.style.display = "none";
+    closeModal();
+});
+
+modalNoButton.addEventListener("click", closeModal);
+
+function closeModal() {
+    backdrop.style.display = "none";
+    modal.style.display = "none";
+};
+
+
+toggleButton.addEventListener("click", function(){
+    mobileNav.style.display = "block";
+    backdrop.style.display = "block";
+});
